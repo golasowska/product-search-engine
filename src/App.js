2fae65b9f7ee58740b78f3ec7faba665d57fc4ba
Anna Golasowska <annagolasowska@gmail.com>
2018-01-12 19:16:51 +0100

    prepared files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Product Search Engine
+      </div>
+    );
+  }
+}
+
+export default App;
