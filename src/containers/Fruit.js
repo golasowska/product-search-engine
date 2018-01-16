25dc17e4deebd03d5f197b86ba214e26b3a72dbd
Anna Golasowska <annagolasowska@gmail.com>
2018-01-16 23:08:00 +0100

    added new container - Category & and minimalize main containers of products, used this.props.history

--- src/containers/Fruit.js
+++ src/containers/Fruit.js
@@ -1,98 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import ReactPaginate from 'react-paginate';
-import _ from 'lodash';
-import * as Actions from '../actions';
+
+import Category from './Category';
 
 
-import Navigation from './Navigation';
-import ShowProduct from './ShowProduct';
-
-class Fruit extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      render: false
-    }
-  }
-
-  componentDidMount=() =>{
-    const value = 'fruit';
-    const selected_page = 0;
-    this.props.fetchCategory(value, selected_page);
-  }
-
-  handlePageClick=(data)=>{
-    const value = 'fruit';
-    let selected_page = data.selected;
-    if (this.state.render) {
-    this.props.sortByPrice(value, selected_page)
-  } else{
-    this.props.fetchCategory(value, selected_page)
-  }
-    this.props.history.push(`/fruit/page=${data.selected+1 }`)
-  }
-
-
-  priceSortDesc=()=>{
-    this.setState({
-      render: true
-    });
-    const value = 'fruit';
-    let selected_page = 0;
-    this.props.sortByPrice(value, selected_page);
-    this.props.history.push(`/fruit/page=${selected_page+1 }`)
-  }
-
-  priceSortAsc=()=>{
-    this.setState({
-      render: false
-    });
-    const value = 'fruit';
-    const selected_page = 0;
-    this.props.fetchCategory(value, selected_page);
-    this.props.history.push(`/fruit/page=${selected_page+1 }`)
-  }
-
-  showData=()=> {
-    return _.map(this.props.data.hits, product=>{
-      return <ShowProduct key ={product._id} product={product} />
-    })
-  }
+export default class Fruit extends React.Component {
 
   render(){
-    const totalHits = this.props.data.total;
-    const pageCount = Math.ceil(totalHits/5)
     return <div>
-    <div>
-      <Navigation />
-    </div>
-    <div>
-      <button className='btn btn-primary' onClick={this.priceSortDesc}>price: highest first</button>
-      <button className='btn btn-primary' onClick={this.priceSortAsc}>price: lowest first</button>
-    </div>
-      {this.showData()}
-      <div>
-      <ReactPaginate previousLabel='<'
-                     nextLabel='>'
-                     breakLabel={<a href="">...</a>}
-                     breakClassName={"break-me"}
-                     marginPagesDisplayed={2}
-                     pageRangeDisplayed={5}
-                     onPageChange={this.handlePageClick}
-                     containerClassName={"pagination"}
-                     subContainerClassName={"pages pagination"}
-                     activeClassName={"active"}
-                     pageCount={pageCount} />
-      </div>
+      <Category category='fruit'
+      history={this.props.history}/>
     </div>
   }
 }
-
-function mapStateToProps(state) {
-  return {
-    data: state.displayCategory
-  }
-}
-
-export default connect(mapStateToProps, Actions) (Fruit);
--- src/containers/Vegetable.js
+++ src/containers/Vegetable.js
@@ -1,63 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import ReactPaginate from 'react-paginate';
-import _ from 'lodash';
-import * as Actions from '../actions';
 
-import Navigation from './Navigation';
-import ShowProduct from './ShowProduct';
+import Category from './Category';
 
-class Vegetable extends React.Component{
 
-  componentDidMount=()=>{
-    const value = 'vegetable';
-    const selected_page = 0;
-
-    this.props.fetchCategory(value, selected_page);
-  }
-
-  handlePageClick=(data)=>{
-    const value = 'vegetable';
-    let selected_page = data.selected;
-    this.props.fetchCategory(value, selected_page);
-    this.props.history.push(`/vegetable/page=${data.selected+1 }`)
-  }
-
-  showData=()=> {
-    return _.map(this.props.data.hits, product => {
-      return <ShowProduct key={product._id} product={product} />
-    })
-  }
+export default class Vegetable extends React.Component {
 
   render(){
-    const totalHits = this.props.data.total;
-    const pageCount = Math.ceil(totalHits/5)
     return <div>
-      <div>
-        <Navigation />
-      </div>
-      {this.showData()}
-      <div>
-      <ReactPaginate previousLabel='<'
-                     nextLabel='>'
-                     breakLabel={<a href="">...</a>}
-                     breakClassName={"break-me"}
-                     marginPagesDisplayed={2}
-                     pageRangeDisplayed={5}
-                     onPageChange={this.handlePageClick}
-                     containerClassName={"pagination"}
-                     subContainerClassName={"pages pagination"}
-                     activeClassName={"active"}
-                     pageCount={pageCount} />
-      </div>
+      <Category category='vegetable'
+      history={this.props.history}/>
     </div>
   }
 }
-
-function mapStateToProps(state) {
-  return {
-    data: state.displayCategory
-  }
-}
-
-export default connect (mapStateToProps, Actions ) (Vegetable);
